bc55494653b62f634b2e6c759efd535cc7919ab5
Natnael Yeshiwas <natiyeshi16@gmail.com>
2024-02-16 12:28:13 +0300

    updating auth

--- app/_components/layout.tsx
+++ app/_components/layout.tsx
@@ -2,13 +2,13 @@
 import React from 'react'
 import client from "../../graphql/client"
 import { ApolloProvider } from '@apollo/client';
-
+let da = 0
 const layout = ({
     children,
   }: Readonly<{
-    children: React.ReactNode;
+    children: React.ReactNode; 
   }>) => {
-    console.log("layout")
+  console.log(da + 1)
   return (
     <ApolloProvider client={client}>
       {children}
