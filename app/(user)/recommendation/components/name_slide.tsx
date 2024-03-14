53bae8d242c081f74d2b4241a4afd35be56fab0b
Natnael Yeshiwas <natiyeshi16@gmail.com>
2024-03-14 20:47:02 +0300

    update

--- app/(user)/recommendation/components/name_slide.tsx
+++ app/(user)/recommendation/components/name_slide.tsx
@@ -1,15 +1,25 @@
 "use client"
 import React,{ useState } from 'react'
+<<<<<<< HEAD:app/(user)/recommendation/components/name_slide.tsx
 import img1 from "../../../assets/images/Group (7).svg"
 import img2 from "../../../assets/images/Group (8).svg"
 import img3 from "../../../assets/images/Group (9).svg"
 import img4 from "../../../assets/images/Group (10).svg"
+=======
+import img1 from "../../assets/images/Group (7).svg"
+import img2 from "../../assets/images/Group (8).svg"
+import img3 from "../../assets/images/Group (9).svg"
+import img4 from "../../assets/images/Group (10).svg"
+>>>>>>> 864bc90 (update):app/recommendation/components/name_slide.tsx
 import Image from 'next/image'
 import Carousel from 'react-multi-carousel'
 import 'react-multi-carousel/lib/styles.css'
 
 const name_slide = () => {
+<<<<<<< HEAD:app/(user)/recommendation/components/name_slide.tsx
     const [selected,setSelected] = useState(-1)
+=======
+>>>>>>> 864bc90 (update):app/recommendation/components/name_slide.tsx
     const responsive = {
         desktop: {
             breakpoint: { max: 4000, min: 1024 },
@@ -39,6 +49,7 @@ const name_slide = () => {
             className='items-center mt-12'
             slidesToSlide={1}
         >   
+<<<<<<< HEAD:app/(user)/recommendation/components/name_slide.tsx
         <div 
             onClick={() => setSelected(0)}
             className={`w-fit cursor-pointer duration-200 p-1 border-2  ${selected == 0 ? ' bg-blue-200 rounded-lg border-blue-600' : 'border-transparent'}`}
@@ -75,6 +86,14 @@ const name_slide = () => {
         >
             <Image src={img2} alt='a' className='h-[4rem] w-[4rem] object-contain' />
         </div>
+=======
+        <Image src={img1} alt='a'  className='cursor-pointer' />
+        <Image src={img2} alt='a'  className='cursor-pointer' />
+        <Image src={img3} alt='a'  className='cursor-pointer' />
+        <Image src={img4} alt='a'  className='cursor-pointer' />
+        <Image src={img1} alt='a'  className='cursor-pointer' />
+        <Image src={img2} alt='a'  className='cursor-pointer' />
+>>>>>>> 864bc90 (update):app/recommendation/components/name_slide.tsx
         
     </Carousel>
         // <div className='p-2 bg-blue-200 cursor-not-allowed rounded-full w-[2em] h-[2em] flex text-white'>
