4bc60da78e244444d14ec870ef583e5aa49f505e
Natnael Yeshiwas <natiyeshi16@gmail.com>
2024-03-20 21:40:11 +0300

    update it

--- app/properties/[list_id]/components/comments.tsx
+++ app/properties/[list_id]/components/comments.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { LuSend } from "react-icons/lu";
 var cms = [
   "Great location and spacious apartment!",
   "The view from the balcony is breathtaking!",
@@ -17,20 +18,32 @@ const AddComment = () => {
   return (
     <div className='flex pb-32 flex-col gap-5'>
 
+      <p className='text-lg'>Leave your comment</p>
       <div className=' flex gap-5'>
-        <div className='flex gap-5 w-1/2'>
+        <div className='flex gap-5 w-full'>
           <div className='w-[3em] flex h-[3em] rounded-full bg-mainBlue'>
               <div className='m-auto text-white'>N</div>
           </div>
-          <input value={comment} onChange={({target} : any) => setComment(target.value)} className='w-full outline-none border-slate-400 rounded-xl bg-white border ps-5' placeholder='insert your text' name='Abcd'  />
+          <div className="flex border w-full border-slate-400 rounded-xl ps-2 bg-white ">
+              <input 
+                value={comment} 
+                onChange={({target} : any) => setComment(target.value)} 
+                className='w-full outline-none  ' 
+                placeholder='insert your text' 
+                name='Abcd'  />
+              <button onClick={()=>{
+              setComments(data => [comment,...data])
+            }} className='px-5 py-5 rounded-lg  flex gap-3'>
+              {/* <p>
+                  Send
+              </p> */}
+              <LuSend className='m-auto text-lg text-mainBlue' />
+            </button>
+          
+          </div>
         </div>
-        <button onClick={()=>{
-          setComments(data => [comment,...data])
-        }} className='bg-mainBlue px-5 py-1 rounded-lg text-white '>
-          Send
-        </button>
-      </div>
-      <div className='flex flex-col gap-4'>
+        </div>
+      <div className='flex flex-col gap-4 ps-5'>
         {comments.map(( val : string ) => <Comment key={val} message={val} />)}
       </div>
     </div>
@@ -38,33 +51,24 @@ const AddComment = () => {
 }
 
 const timeStamp  = () => {
-  // Assuming you have a timestamp in milliseconds
-  // Assuming you have a timestamp in milliseconds
   var timestamp = 1647723600000;
-
-  // Create a new Date object using the timestamp
   var date = new Date(timestamp);
-
-  // Extract the time and format it
   var time = date.toLocaleTimeString(); // Adjust the format based on your preference
-
-  // Extract the date and format it
-  var options = { year: 'numeric', month: 'long', day: 'numeric' };
   var formattedDate = date.toLocaleDateString(); // Adjust the format based on your preference
   return formattedDate
 }
 const Comment = ({ message } : { message : string }) => {
   const time = timeStamp()
-  return <div className='flex  gap-5'>
+  return <div className='flex gap-5 '>
     <div className='w-[3em] flex h-[3em] rounded-full bg-mainBlue'>
         <div className='m-auto text-white'>N</div>
     </div>
-    <div className='flex gap-2'>
-      <div className='w-fit px-4 flex align-middle place-content-center place-items-center rounded-lg bg-white'>
-        {message}
+      <div className='py-2 flex flex-col break-words flex-wrap px-4 w-fit  rounded-lg bg-white'>
+        <div>
+          {message}
+        </div>
+        <div className='text-sm mt-2 text-gray-500 place-self-end'>{time}</div>
       </div>
-      <div className='place-content-end place-self-end text-sm'>{time}</div>
-    </div>
   </div>
 }
 
