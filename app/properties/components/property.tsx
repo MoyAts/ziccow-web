88b203b892aebe3264e6a9eef78eb6a738c5a025
Natnael Yeshiwas <natiyeshi16@gmail.com>
2024-04-09 22:44:12 +0300

    update pretify

--- app/properties/components/property.tsx
+++ app/properties/components/property.tsx
@@ -14,6 +14,7 @@ import Link from "next/link";
 import { useSelector } from "react-redux";
 import { LogInf, getState } from "@/store/features/auth/authSlice";
 import { FaStar as StarIcon } from "react-icons/fa6";
+import BuildStar from "./buildstar";
 interface Props {
   house: houseInf;
   userId: string | null;
