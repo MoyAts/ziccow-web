aeccdb789eef6279505acb4f3efd8bc0a572482e
Natnael Yeshiwas <natiyeshi16@gmail.com>
2024-04-29 23:38:57 +0300

    update data

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <NextTopLoader />
+        <NextTopLoader zIndex={100000} />
         <Provider>{children}</Provider>
       </body>
     </html>
