c85d24a3956000965ecf0764a73ae901ed8d7b2b
amenabe22 <bdere12345@gmail.com>
2024-05-03 23:33:17 +0300

    typo fix

--- app/recommendation/components/realestates.tsx
+++ app/recommendation/components/realestates.tsx
@@ -129,7 +129,7 @@ const Realestates = ({ fromHome = false }: { fromHome?: boolean }) => {
       <div className="h-fit w-full max-w-[1700px] pb-20 mx-auto px-20  max-tablet:px-5 pt-10">
         <div className="flex  flex-col gap-7 max-tablet:flex-col max-tablet:items-center">
           <h1 className="flex gap-2 text-3xl max-tablet:text-xl max-tablet:mb-5">
-            <span className="">Real Estats</span>
+            <span className="">Real Estates</span>
           </h1>
           {propertyType && (
             <h1 className="flex gap-2 mt-5 text-2xl max-tablet:text-xl max-tablet:mb-5">
