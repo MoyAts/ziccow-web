439a74ce960df3c428e36aea2c653cf60f6075d0
Natnael Yeshiwas <natiyeshi16@gmail.com>
2024-05-07 12:00:58 +0300

    temp

--- app/properties/[list_id]/components/matterPortEmbed.tsx
+++ app/properties/[list_id]/components/matterPortEmbed.tsx
@@ -1,11 +1,49 @@
 "use client";
 
 const matterPortEmbed = ({ link, height }: any) => {
+  const yt1 = check_for_share_youtube_link(link) 
+  const yt2 = check_for_normal_youtube_link(link) 
   return (
     <div className="mt-4">
-      <iframe src={link} width={"100%"} height={height}></iframe>
+      {link.startsWith("<div") ? 
+        <>
+          <div dangerouslySetInnerHTML={{ __html: link }} />   
+        </>
+        :
+      <iframe src={yt1 ?? yt2 ?? link} width={"100%"} height={height}></iframe>
+      }
     </div>
   );
 };
+
+const check_for_share_youtube_link = (link : string) => {
+  let ind = link.indexOf("youtu.be/")
+  let id = ""
+  if(ind == -1) return null
+  let st = ind + 9 
+  while(st < link.length){
+    if (link[st] == "?"){
+      break
+    }
+    id += link[st]
+    st += 1
+  }
+  
+  return "https://www.youtube.com/embed/" + id
+}
+const check_for_normal_youtube_link = (link : string) => {
+  let ind = link.indexOf("youtube.com/watch?v=")
+  let id = ""
+  if(ind == -1) return null
+  let st = ind + 20 
+  while(st < link.length){
+    if (link[st] == "?"){
+      break
+    }
+    id += link[st]
+    st += 1
+  }
+  return "https://www.youtube.com/embed/" + id
+}
 
 export default matterPortEmbed;
